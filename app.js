abaf76cc528277526420434021aef39bfab6042d
Andy Nguyen <andy123052@gmail.com>
2021-03-11 23:18:58 +0700

    final

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
-let songTitles = ["Alive","Dead","Blackbird","Friday"];
+let songs = ['Alive', 'Dead', 'Blackbird', 'Friday', 'On the Rocks'];
 
-let i = 0;
-arr.map(n => {
-  n['id'] ="song"+ i;
-  i++;
+let paragraph;
+//let container = document.getElementById('container');
+//let container = document.querySelector('#container');
+for (let i = 0; i < songs.length; i++) {
+  paragraph = document.createElement('p');
+  paragraph.setAttribute('id', `song${i + 1}`);
+  paragraph.innerText = songs[i];
+  //container.appendChild(paragraph);
+  document.querySelector('#container').appendChild(paragraph);
+}
+
+songs.forEach((s) => {
+  paragraph = document.createElement('p');
+  paragraph.setAttribute('id', `song${songs.indexOf(s) + 1}`);
+  paragraph.innerText = s;
+  //container.appendChild(paragraph);
+  document.querySelector('#container').appendChild(paragraph);
 });
 
-let div = document.getElementById("container");
-div.appendChild(songTitles);
-let newUl = document.createElement('ul');
-songTitles.forEach(element => {document.createElement('li')
-});
-
-newUl.appendChild(songTitles);
-div.appendChild(newUl);
+let ul = document.createElement('ul');
+let li;
+for (let i = 0; i < songs.length; i++) {
+  li = document.createElement('li');
+  li.setAttribute('id', `song${i + 1}`);
+  li.innerText = songs[i];
+  //container.appendChild(paragraph);
+  document.querySelector('#container').appendChild(li);
